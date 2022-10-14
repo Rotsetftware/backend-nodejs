fc466a0276b34f036f700cfd4204d987581bfadb
Rodolfo Braquetes <braquetesss@gmail.com>
2022-10-14 17:34:49 -0500

    first commit

---
+++ src/database.js
@@ -0,0 +1,29 @@
+const mysql = require("mysql");
+const fs = require('fs');
+
+const mysqlConnection = mysql.createConnection({
+
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'videojuego'
+
+  host:"asesorias.mysql.database.azure.com",
+  user:"braquetes",
+  password:"Rodo2507137946",
+  database:"videojuego",
+  port:3306,
+  ssl:{ca:fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
+
+});
+
+mysqlConnection.connect(function(err) {
+    if (err) {
+        console.error(err);
+        return;
+    } else {
+        console.log("Database is connected");
+    }
+});
+
+module.exports = mysqlConnection;
